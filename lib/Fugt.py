d32bdcfc20aa60c1238f085c91238c40e3255aee
Mig <victorhogue@gmail.com>
2025-04-04 12:01:39 +0200

    Added fugtighed sensor

---
+++ lib/Fugt.py
@@ -0,0 +1,32 @@
+import smbus
+import time
+
+class MCP3021:
+    bus = smbus.SMBus(1)
+   
+    def __init__(self, address = 0x4B):
+        self.address = address
+   
+    def read_raw(self):
+        # Reads word (16 bits) as int
+        rd = self.bus.read_word_data(self.address, 0)
+        # Exchanges upper and lower bytes
+        data = ((rd & 0xFF) << 8) | ((rd & 0xFF00) >> 8)
+        # Ignores two least significiant bits
+        return data >> 2
+
+    def fugt_procent(self):
+        adc = self.read_raw()
+        procent = ((780-adc)/(780-308))*100
+        if procent < 0:
+            procent = 0
+        return procent
+"""
+adc = MCP3021()
+
+while True:
+    raw = adc.read_raw()
+    print("Raw :", raw)
+    print("procent :", adc.fugt_procent())
+    time.sleep(1)
+"""
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from time import sleep
 from lib.led_light import LED_sun
 from lib.pumpe import Pumpe
+from lib.Fugt import MCP3021
 
 pumpe = Pumpe(16)
-led_sun = LED_sun(12, 13)
+led_sun = LED_sun(12, 13)   
+fugt_sensor = MCP3021()
 
 led_sun.set_duty_RED(60)
 led_sun.set_duty_BLUE(77)
 
 while True:
-    led_sun.print_duty()
-    pumpe.turnOn()
-    sleep(1)
-    pumpe.turnOff()
-    sleep(1)
+    procent = fugt_sensor.fugt_procent()
+    if procent < 10:
+        pumpe.turnOn()
+    elif procent > 60:
+        pumpe.turnOff()
+    sleep(3)
