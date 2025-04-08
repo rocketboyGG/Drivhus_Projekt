585c2d1341cbee441926c778fa7cc7a3d6025e04
GithubSadness <philiphansen080601@gmail.com>
2025-04-08 12:01:29 +0200

    schedule test
    schedule test til fugtcheck

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib.led_light import LED_sun
 from lib.pumpe import Pumpe
 from lib.fugt_sensor import MCP3021
 from lib.lys_sensor import LysSensor
+import schedule
 
 pumpe = Pumpe(16)
 led_sun = LED_sun(12, 13)   
@@ -14,13 +15,26 @@ led_sun.set_duty_BLUE(77)
 
 fugt_time = time()
 
+def fugtcheck():
+    print("checker fugt!")
+    if procent < 60:
+        print("tænder pumpe!")
+        pumpe.turnOn()
+        sleep(2)
+        pumpe.turnOff()
+
+schedule.every(1).minutes.do(fugtcheck)
+
 while True:
     procent = fugt_sensor.fugt_procent()
     print("Fugt Procent", procent)
     print("Lys ", lys_sen.read_raw())
+    """
     if time() - fugt_time >= 10:
         if procent < 60:
             pumpe.turnOn()
             sleep(2)
             pumpe.turnOff()
+            """
+    schedule.run_pending()
     sleep(3)
