75fbf79bfd86d667f4fe00155e1435c0395cbd3c
GithubSadness <philiphansen080601@gmail.com>
2025-04-08 13:36:09 +0200

    web stuff
    yep

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ lys_sen = LysSensor()
 led_sun.set_duty_RED(60)
 led_sun.set_duty_BLUE(77)
 
-fugt_time = time()
 
 def fugtcheck():
     print("checker fugt!")
@@ -29,12 +28,5 @@ while True:
     procent = fugt_sensor.fugt_procent()
     print("Fugt Procent", procent)
     print("Lys ", lys_sen.read_raw())
-    """
-    if time() - fugt_time >= 10:
-        if procent < 60:
-            pumpe.turnOn()
-            sleep(2)
-            pumpe.turnOff()
-            """
     schedule.run_pending()
     sleep(3)
